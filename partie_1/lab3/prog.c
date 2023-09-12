f04872348f1f540c3f5dbe6f1b6fc88bd66bbeb9
Chems-bitti <shemseddinenaimi@gmail.com>
2023-09-12 12:36:37 +0200

    Added different programs done on Lab 3

---
+++ partie_1/lab3/prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+    int x,y;
+
+    printf("Enter x :");
+    scanf("%d", &x);    
+    printf("Enter y :");
+    scanf("%d", &y);    
+
+    printf("x*y = %d\n", x*y);
+
+}
